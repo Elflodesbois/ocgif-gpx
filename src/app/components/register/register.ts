42efd25d79ed3ade56a2c564c88144d65dc56d92
Chloe <chloe@goofy.fr>
2026-02-13 18:38:10 +0100

    login et register en matérial

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,23 +1,40 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Auth } from '../../services/auth';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NavbarModeSelector } from '../../services/navbar-mode-selector';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+
   mode = inject(NavbarModeSelector);
- 
+
   username = '';
   password = '';
+  confirmPassword = '';
   error = '';
+
+  hidePassword = true;
+  hideConfirmPassword = true;
 
   constructor(
     private auth: Auth,
@@ -25,6 +42,13 @@ export class Register {
   ) {}
 
   register() {
+
+    // Vérification mot de passe
+    if (this.password !== this.confirmPassword) {
+      this.error = "Les mots de passe ne correspondent pas";
+      return;
+    }
+
     this.auth.register(this.username, this.password).subscribe({
       next: () => {
         alert('Compte créé avec succès');
